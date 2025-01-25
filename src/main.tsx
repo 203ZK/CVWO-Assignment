e8228ab637ad8607750f57e7fc3caa44b4f46036
203ZK <houjinwork@gmail.com>
2025-01-25 20:51:25 +0800

    Use hashrouter instead of browserrouter

--- src/main.tsx
+++ src/main.tsx
@@ -1,6 +1,6 @@
 import { StrictMode } from 'react'
 import { createRoot } from 'react-dom/client'
-import { createBrowserRouter, RouterProvider } from "react-router"
+import { createHashRouter, RouterProvider } from "react-router"
 
 import Login from "./components/pages/Login"
 import Register from "./components/pages/Register"
@@ -9,7 +9,7 @@ import CreateThread from "./components/pages/CreateThread"
 import EditThread from "./components/pages/EditThread"
 import ThreadPost from "./components/pages/ThreadPost"
 
-const router = createBrowserRouter([
+const router = createHashRouter([
   {
     path: "/",
     element: <Login />
