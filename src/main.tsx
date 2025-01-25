7b91457ed44db021bbd80b5ac3ffaf9d3acd9644
203ZK <houjinwork@gmail.com>
2025-01-25 20:43:02 +0800

    Refactor App.tsx

--- src/main.tsx
+++ src/main.tsx
@@ -1,9 +1,47 @@
 import { StrictMode } from 'react'
 import { createRoot } from 'react-dom/client'
-import App from './App.tsx'
+import { createBrowserRouter, RouterProvider } from "react-router"
+
+import Login from "./components/pages/Login"
+import Register from "./components/pages/Register"
+import Forum from "./components/pages/Forum"
+import CreateThread from "./components/pages/CreateThread"
+import EditThread from "./components/pages/EditThread"
+import ThreadPost from "./components/pages/ThreadPost"
+
+const router = createBrowserRouter([
+  {
+    path: "/",
+    element: <Login />
+  },
+  {
+    path: "register",
+    element: <Register />
+  },
+  { 
+    path: "forum",
+    element: <Forum />
+  },  
+  {
+    path: "create-thread",
+    element: <CreateThread />
+  },
+  {
+    path: "edit-thread/:thread_id",
+    element: <EditThread />
+  },
+  {
+    path: "thread/:thread_id",
+    element: <ThreadPost />
+  },
+  {
+    path: "*",
+    element: <p>Error!</p>
+  }
+]);
 
 createRoot(document.getElementById('root')!).render(
   <StrictMode>
-    <App />
+    <RouterProvider router={router} />
   </StrictMode>,
 )
