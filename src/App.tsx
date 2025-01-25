9f1d0c033515aa4a318a7436d428f581bd029f7e
203ZK <houjinwork@gmail.com>
2025-01-25 12:03:33 +0800

    Reorganised.

--- src/App.tsx
+++ src/App.tsx
@@ -1,11 +1,11 @@
 import { BrowserRouter, Route, Routes } from "react-router"
 
-import Login from "../components/pages/Login"
-import Register from "../components/pages/Register"
-import Forum from "../components/pages/Forum"
-import CreateThread from "../components/pages/CreateThread"
-import EditThread from "../components/pages/EditThread"
-import ThreadPost from "../components/pages/ThreadPost"
+import Login from "./components/pages/Login"
+import Register from "./components/pages/Register"
+import Forum from "./components/pages/Forum"
+import CreateThread from "./components/pages/CreateThread"
+import EditThread from "./components/pages/EditThread"
+import ThreadPost from "./components/pages/ThreadPost"
 
 function App() {
   return (
